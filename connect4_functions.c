2a0f4d62dec2f8cfbb9791da487f7d0f971d2ce0
Jeff Yu <yyu72@ucsc.edu>
2020-06-02 16:06:30 -0700

    Changing Tie Game format, adding check_winner function, print_board format 2 \n

--- connect4_functions.c
+++ connect4_functions.c
@@ -6,10 +6,11 @@
 #define BOARD_SIZE_HORIZ 7
 #define BOARD_SIZE_VERT 6
 
+#include "connect4_functions.h"
 
 int print_welcome(void) {
 	printf("*** Welcome to the Connect Four game!!! ***\n");
-	printf("Weould you like to make the first move [y/n]: ");
+	printf("Would you like to make the first move [y/n]: ");
 	char input_move;
 	scanf("%c", &input_move);
 	while (getchar() != '\n');
@@ -20,8 +21,6 @@ int print_welcome(void) {
 
 void display_board(int board[][BOARD_SIZE_VERT]){
     int col, row;
-    //board[3][3] = 1;
-    //board[5][2] = 2;
     for (row = 0; row < BOARD_SIZE_VERT; row++){
         for (col = 0; col < BOARD_SIZE_HORIZ; col++){
             printf("+---");
@@ -38,7 +37,7 @@ void display_board(int board[][BOARD_SIZE_VERT]){
         printf("|\n");
     }
     printf("+---+---+---+---+---+---+---+\n");
-    printf("  1   2   3   4   5   6   7  \n");
+    printf("  1   2   3   4   5   6   7  \n\n");
 }
 
 void update_board(int board[][BOARD_SIZE_VERT], int m, int player_num){
@@ -82,32 +81,33 @@ again:
 
 int player_move(int board[][BOARD_SIZE_VERT], int player_num){
     //int valid = 1;
-    int vertical_position = 0;
     int player_move;
     while (1){
         printf("Please enter your move: ");
         scanf("%d", &player_move);
-        if (is_column_full(board, player_move)){
-            printf("Not a valid move. Enter a column number!\n");
+        while (getchar() != '\n');
+        
+        if (player_move > 0 && player_move <=BOARD_SIZE_HORIZ &&  is_column_full(board, player_move)){//column is full
+            printf("Not a valid move. This column is full. Try again!\n");
             continue;
         }
             
-        if (player_move <= BOARD_SIZE_HORIZ && player_move > 0 && is_column_full(board, player_move == 0))
+        if (player_move <= BOARD_SIZE_HORIZ && player_move > 0 && is_column_full(board, player_move == 0))// if valid move
             break;
-        while (getchar() != '\n');
+        
         printf("Not a valid move. Enter a column number!\n");
     }
     
-    for (int j = 0; j < BOARD_SIZE_VERT; j++){//gets the vertical position of the empty cell
-        if (board[player_move - 1][j] == 0){
-            vertical_position = j;
-            break;
-        }
-    }
     update_board(board, player_move, player_num);
     return player_move;
 }
 
+int check_winner(int board[][BOARD_SIZE_VERT], int last_move){
+    if (last_move == 1){
+        return 1;
+    }
+    return 2;
+}
 
 bool check_win_or_tie(int board[][BOARD_SIZE_VERT], int last_move){
     int non_empty_space = 0;
@@ -118,31 +118,43 @@ bool check_win_or_tie(int board[][BOARD_SIZE_VERT], int last_move){
             }
             if (board[j][i] != 0 && board[j][i] == board[j + 1][i] && board[j + 1][i] == board[j + 2][i] && board[j + 2][i] == board[j + 3][i]){//check horizontal for win
                 if (board[j][i] == 1){
-                    printf("Player X won!\n");
+                    printf("*****************************\n");
+                    printf("* Player X won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 }
                 if (board[j][i] == 2){
-                    printf("Player O won!\n");
+                    printf("*****************************\n");
+                    printf("* Player O won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 }
             }
             else if (board[j][i] != 0 && board[j][i] == board[j][i + 1] && board[j][i + 1] == board[j][i + 2] && board[j][i + 2] == board[j][i + 3]){//check vertical
                 if (board[j][i] == 1){
-                    printf("Player X wins!\n");
+                    printf("*****************************\n");
+                    printf("* Player X won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 }
                 if (board[j][i] == 2){
-                    printf("Player O wins!\n");
+                    printf("*****************************\n");
+                    printf("* Player O won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 }
             }
             else if (board[j][i] != 0 && board[j][i] == board[j + 1][i + 1] && board[j + 1][i + 1] == board[j + 2][i + 2] && board[j + 2][i + 2] == board[j + 3][i + 3]){
                 if (board[j][i] == 1){
-                    printf("Player X wins!\n");
+                    printf("*****************************\n");
+                    printf("* Player X won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 }
                 if (board[j][i] == 2){
-                    printf("Player O wins!\n");
+                    printf("*****************************\n");
+                    printf("* Player O won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 } //check diagnoal 1
             }
@@ -152,18 +164,24 @@ bool check_win_or_tie(int board[][BOARD_SIZE_VERT], int last_move){
         for (int j = BOARD_SIZE_VERT; j >= 3; j--){
             if (board[j][i] == board[j - 1][i + 1] && board[j - 1][i + 1] == board[j - 2][i + 2] && board[j - 2][i + 2] == board[j - 3][i + 3]){
                 if (board[j][i] == 1){
-                    printf("Player X wins!\n");
+                    printf("*****************************\n");
+                    printf("* Player X won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 }
                 if (board[j][i] == 2){
-                    printf("Player O wins!\n");
+                    printf("*****************************\n");
+                    printf("* Player O won!!! Game over *\n");
+                    printf("*****************************\n");
                     return true;
                 }
             }
         }
     }
     if (non_empty_space == BOARD_SIZE_VERT * BOARD_SIZE_HORIZ){
-        printf("Tie game!");
+        printf("*****************************\n");
+        printf("* Game is a tie!! No winner *\n");
+        printf("*****************************\n");
         return true;
     }
     return false;
